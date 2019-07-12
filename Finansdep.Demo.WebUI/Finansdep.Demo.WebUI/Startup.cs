9dd107836776e01dbe636fae573838c9f5db15e2
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2019-07-12 17:09:03 +0200

    Downgrade to .NET Core 2.2 to allow automatic deployment to Azure.

--- Finansdep.Demo.WebUI/Finansdep.Demo.WebUI/Startup.cs
+++ Finansdep.Demo.WebUI/Finansdep.Demo.WebUI/Startup.cs
@@ -21,8 +21,8 @@ namespace Finansdep.Demo.WebUI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc()
-				.AddNewtonsoftJson();
+			services.AddMvc();
+			//	.AddNewtonsoftJson();
 
 			// In production, the Angular files will be served from this directory
 			services.AddSpaStaticFiles(configuration =>
@@ -32,7 +32,7 @@ namespace Finansdep.Demo.WebUI
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
